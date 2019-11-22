c3e0349a606c0b858e4819b7fca7c69271c7af34
KimDuer <kimcodesjs@gmail.com>
2019-11-21 22:44:18 -0600

    moved sis title to navpanel

--- src/components/NavPanel.js
+++ src/components/NavPanel.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
-const NavPanel = () => {
-  
+const NavPanel = (props) => {
   return (
   <div>
+  <h1>Serenity in Soap</h1>
     <ul>
         <li>
-          <Link to="artisan">Artisan Soaps</Link>
+          <Link 
+            to='/artisan'>
+            Artisan Soaps
+          </Link>
         </li>
         <li>
-          <Link to="/classic">Classic Soaps</Link>
+          <Link 
+            to='/classic'>
+            Classic Soaps
+          </Link>
         </li>
         <li>
-          <Link to="/">Home</Link>
+          <Link 
+            to="/">
+            Home
+          </Link>
         </li>
     </ul>
   </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 const Welcome = () => (
     <div>
-        <h1>Serenity in Soap</h1>
         <h2>Welcome!</h2>   
     </div>
 )
