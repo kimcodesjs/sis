fad4154ceb1b0c8329604dd89e06a367d4580cec
KimDuer <kimcodesjs@gmail.com>
2019-12-09 11:18:54 -0600

    added scrolltotoponmount, added price

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react'
 import { useParams, withRouter } from 'react-router-dom'
 import artisanItems from '../data/Artisan'
+import ScrollToTopOnMount from './ScrollToTopOnMount'
 
 
-const Item = (props) => {
+const Item = () => {
     let category = useParams().category
     if (category === 'artisan') {
-        const item = artisanItems.find(item => item.title === useParams().item)
+        const item = artisanItems.find(item => item.img === useParams().item)
         return (
             <div>
+            <ScrollToTopOnMount />
             <h2>{item.title}</h2>
             <h3>{item.description}</h3>
-            <img src={window.location.origin + '/img/'+category+'/'+item.title+'.jpeg'} />
+            <h3>${item.price}.00</h3>
+            <img src={window.location.origin + '/img/'+category+'/'+item.img+'.jpeg'} />
             </div>
         )
     }
