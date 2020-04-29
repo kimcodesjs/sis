f42a25a72929dd1ddb8664cd1c387ddedf7af474
KimDuer <kimcodesjs@gmail.com>
2020-04-29 07:11:45 -0500

    linked to and read from database

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,20 +1,65 @@
 import React from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { Link, useRouteMatch, useParams } from 'react-router-dom'
+import { firestoreConnect } from 'react-redux-firebase'
+
 const imgStyle = {
     width: '60%'
-}  
+} 
+
 const Items = (props) => {
+
     let { url } = useRouteMatch();
-    
-    const itemImgList = props.items.map(item =>
-        <li key={item}>
-            <Link 
-                to={url+'/'+item}>
-                <img style={imgStyle} src={window.location.origin + '/img/'+props.category+'/'+item+'.jpeg'} />
-            </Link>
-        </li>
-        )
-    return <ul>{itemImgList}</ul>
+    let collection = useParams().collection
+    let itemList = []
+    if (collection === 'Tier 1') {
+        itemList = props.itemsT1.map((item) => {
+          return (
+            <li key={item.img}>
+                <Link 
+                    to={url+'/'+item.img}
+                    onClick={e => {
+                        props.updateActiveItem(item)}}>
+                    <img style={imgStyle} src={window.location.origin + '/img/'+collection+'/'+item.img+'.jpeg'} />
+                </Link>
+            </li>
+          )  
+        })
+    } else if (collection === 'Tier 2') {
+        itemList = props.itemsT2.map((item) => {
+            return (
+              <li key={item.img}>
+                  <Link 
+                      to={url+'/'+item.img}
+                      onClick={e => {
+                        props.updateActiveItem(item)}}>
+                      <img style={imgStyle} src={window.location.origin + '/img/'+collection+'/'+item.img+'.jpeg'} />
+                  </Link>
+              </li>
+            )  
+        })
+    }
+    // why does this log twice?
+    console.log(props.t1)
+    return <ul>{itemList}</ul>
 }
 
-export default Items
+const mapStateToProps = state => {
+    return {
+        itemsT1: state.firestore.ordered.tier1 ? state.firestore.ordered.tier1.map(item => item) : [],
+        itemsT2: state.firestore.ordered.tier2 ? state.firestore.ordered.tier2.map(item => item) : [],
+        t1: state.firestore.ordered.tier1
+    }
+}
+// persist state thru page refresh?
+const mapDispatchToProps = dispatch => {
+    return {
+        updateActiveItem: (item) => dispatch({ type: 'UPDATE_ACTIVE_ITEM', item})
+    }
+}
+
+export default compose(
+    firestoreConnect(() => ['tier1', 'tier2']),
+    connect(mapStateToProps, mapDispatchToProps)
+)(Items)
