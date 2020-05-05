9979b1821cfba9b9b85d27fa8bd94e1fca153c27
KimDuer <kimcodesjs@gmail.com>
2020-05-05 00:55:19 -0500

    removed data passed in props

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,36 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import Items from './Items'
-import Item from './Item'
+import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 
 const Gallery = (props) => {
-    let { category } = useParams();
-    if (category === 'artisan') {
-        return (
-            <div>
-                <h1>Artisan Soaps</h1> 
-                <Items 
-                    items={['aspirations', 'black-raspberry-vanilla', 'black-tie',
-                    'celestial-waters', 'cotton-fields', 'dragon\'s-blood',
-                    'green-smoothie', 'heavenly-bouquet', 'mulberry']}
-                    category={category}
-                    location={props.location}/>
-            </div>
-        ) 
-    } if (category === 'classic') {
-        return (
-            <div>
-                <h1>Classic Soaps</h1> 
-                <Items 
-                    items={['northern-lights', 'oatmeal-almond-milk', 'oatmeal-almond-milk-honey',
-                    'outdoors', 'peppermint-swirl', 'pumpkin-pie', 'summertime']}
-                    category={category}
-                    location = {props.location}/>
-            </div>
-        )
+    
+    let collection = useParams().collection;
+    
+    useEffect(() => {
+        props.updateCollection(collection)
+    })
+    return (
+        <div>
+            <h1>{collection}</h1> 
+            <Items 
+                collection={collection}
+                location={props.location}/>
+        </div>
+    ) 
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        updateCollection: (collection) => dispatch({ type: 'UPDATE_COLLECTION', collection })
     }
 }
 
-
-export default Gallery
+export default connect(null, mapDispatchToProps)(Gallery)
